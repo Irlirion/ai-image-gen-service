6b34863338818f4edde58af2731912b33b7406e5
Grigorev Rostislav <grigorjew.rostislaw@gmail.com>
2025-01-10 19:53:53 +0300

    remove img2img

--- service.py
+++ service.py
@@ -1,57 +1,13 @@
+import base64
+import io
+
 import bentoml
-import PIL
-import PIL.Image
 from bentoml.models import HuggingFaceModel
 from PIL.Image import Image
 
 sample_prompt = (
     "A cinematic shot of a baby racoon wearing an intricate italian priest robe."
 )
-
-
-@bentoml.service(
-    traffic={
-        "timeout": 300,
-    },
-    workers=1,
-    resources={
-        "gpu": 1,
-    },
-)
-class FluxImage2Image:
-    model_path = HuggingFaceModel("black-forest-labs/FLUX.1-dev")
-
-    def __init__(self) -> None:
-        import torch
-        from diffusers.pipelines.flux.pipeline_flux_img2img import FluxImg2ImgPipeline
-
-        torch.set_float32_matmul_precision("high")
-
-        self.pipe = FluxImg2ImgPipeline.from_pretrained(
-            self.model_path, torch_dtype=torch.bfloat16
-        ).to(device="cuda")
-
-        self.pipe = optimize(self.pipe)
-
-    @bentoml.api
-    async def predict(
-        self,
-        image: Image,
-        prompt: str,
-        num_inference_steps: int = 50,
-        strength: float = 0.6,
-        guidance_scale: float = 3.5,
-    ) -> Image:
-        image = self.pipe(
-            prompt=prompt,
-            image=image,
-            num_inference_steps=num_inference_steps,
-            guidance_scale=guidance_scale,
-            strength=strength,
-            height=image.height,
-            width=image.width,
-        ).images[0]
-        return image
 
 
 @bentoml.service(
@@ -86,15 +42,24 @@ class FluxText2Image:
         guidance_scale: float = 3.5,
         height: int = 1024,
         width: int = 1024,
-    ) -> Image:
-        image = self.pipe(
+    ) -> dict[str, list[str]]:
+        def encode_image(image: Image) -> str:
+            buffer = io.BytesIO()
+            image.save(buffer, "JPEG")
+            image_bytes = buffer.getvalue()
+            encoded_image = base64.b64encode(image_bytes).decode("utf-8")
+            return encoded_image
+
+        images: list[Image] = self.pipe(
             prompt=prompt,
             num_inference_steps=num_inference_steps,
             guidance_scale=guidance_scale,
             height=height,
             width=width,
-        ).images[0]
-        return image
+            max_sequence_length=512,
+            num_images_per_prompt=4,
+        ).images
+        return {"images": [encode_image(image) for image in images]}
 
 
 @bentoml.service(
@@ -105,7 +70,6 @@ class FluxText2Image:
 )
 class Flux:
     text2img_service = bentoml.depends(FluxText2Image)
-    img2img_service = bentoml.depends(FluxImage2Image)
 
     @bentoml.api
     async def txt2img(
@@ -115,39 +79,18 @@ class Flux:
         guidance_scale: float = 3.5,
         height: int = 1024,
         width: int = 1024,
-    ) -> Image:
-        image = await self.text2img_service.predict(
+    ) -> dict[str, list[str]]:
+        return await self.text2img_service.predict(
             prompt=prompt,
             num_inference_steps=num_inference_steps,
             guidance_scale=guidance_scale,
             height=height,
             width=width,
         )
-        return image
-
-    @bentoml.api
-    async def img2img(
-        self,
-        prompt: str,
-        image: Image,
-        num_inference_steps: int = 50,
-        strength: float = 0.6,
-        guidance_scale: float = 3.5,
-    ) -> Image:
-        image = await self.img2img_service.predict(
-            prompt=prompt,
-            image=image,
-            num_inference_steps=num_inference_steps,
-            strength=strength,
-            guidance_scale=guidance_scale,
-        )
-        return image
 
 
 def optimize(pipe, compile=True):
     import torch
-    from diffusers.pipelines.flux.pipeline_flux import FluxPipeline
-    from diffusers.pipelines.flux.pipeline_flux_img2img import FluxImg2ImgPipeline
 
     # fuse QKV projections in Transformer and VAE
     pipe.transformer.fuse_qkv_projections()
@@ -179,21 +122,15 @@ def optimize(pipe, compile=True):
 
     # trigger torch compilation
     print("🔦 running torch compiliation (may take up to 20 minutes)...")
-    if isinstance(pipe, FluxImg2ImgPipeline):
-        pipe(
-            "dummy prompt to trigger torch compilation",
-            image=PIL.Image.new("RGB", (1024, 1024)),
-            output_type="pil",
-            num_inference_steps=50,  # use ~50 for [dev], smaller for [schnell]
-        ).images[0]
-    elif isinstance(pipe, FluxPipeline):
-        pipe(
-            "dummy prompt to trigger torch compilation",
-            output_type="pil",
-            num_inference_steps=50,  # use ~50 for [dev], smaller for [schnell]
-        ).images[0]
-    else:
-        raise ValueError("Unsupported pipeline type, received %s" % type(pipe))
+    pipe(
+        "dummy prompt to trigger torch compilation",
+        output_type="pil",
+        num_inference_steps=50,  # use ~50 for [dev], smaller for [schnell]
+        height=1024,
+        width=1024,
+        num_images_per_prompt=4,
+        max_sequence_length=512,
+    ).images[0]
 
     print("🔦 finished torch compilation")
 
